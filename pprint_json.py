b587ba086a33864e40a8325873a153a9b40b8537
denis5417 <denisz.pol@yandex.ru>
2017-07-19 07:48:51 +0300

    Добавлена защита от дурака

--- pprint_json.py
+++ pprint_json.py
@@ -1,14 +1,27 @@
 import json
 import sys
+import os
+
 
 def load_data(filepath):
-    with open(filepath) as handle:
-        parsed_json = json.load(handle)
-    return parsed_json
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath) as file_handler:
+        try:
+            return json.load(file_handler)
+        except json.decoder.JSONDecodeError:
+            return None
+
 
 def pretty_print_json(parsed_json):
-    print(json.dumps(parsed_json, indent=4, sort_keys=True))
+    if not parsed_json:
+        print("Неверный путь к файлу, или в файле не JSON")
+    else:
+        print(json.dumps(parsed_json, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data(sys.argv[1]))
+    try:
+        pretty_print_json(load_data(sys.argv[1]))
+    except IndexError:
+        print("Укажите путь к файлу")
