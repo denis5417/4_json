dc15cb1270d39710d56591c90cffb63048adcdaa
denis5417 <denisz.pol@yandex.ru>
2017-07-20 10:00:16 +0300

    Убрано исключение из ifmain, pass заменен на return None

--- pprint_json.py
+++ pprint_json.py
@@ -14,13 +14,13 @@ def load_data(filepath):
 
 def pretty_print_json(parsed_json):
     if not parsed_json:
-        pass
+        return None
     else:
         print(json.dumps(parsed_json, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    try:
+    if len(sys.argv) > 1:
         pretty_print_json(load_data(sys.argv[1]))
-    except IndexError:
+    else:
         print("Укажите путь к файлу")
